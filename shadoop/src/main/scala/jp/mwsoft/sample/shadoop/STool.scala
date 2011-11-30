4c18e2d2056b14dfb83cffccc4245fe7bcf4c47d
masato <masato@usagi.(none)>
2011-12-01 01:41:49 +0900

    delete setOutput/InputFormat

--- shadoop/src/main/scala/jp/mwsoft/sample/shadoop/STool.scala
+++ shadoop/src/main/scala/jp/mwsoft/sample/shadoop/STool.scala
@@ -10,8 +10,8 @@ import org.apache.hadoop.util.{ Tool, ToolRunner }
 trait STool extends Configured with Tool {
 
   /** 標準のmainメソッドも用意しておく */
-  def main(args: Array[String]) {
-    exit(ToolRunner.run(this, args))
+  def main( args: Array[String] ) {
+    exit( ToolRunner.run( this, args ) )
   }
 
   /** 継承先のクラスはClassはMapperを設定すること */
@@ -26,52 +26,43 @@ trait STool extends Configured with Tool {
   // Comparetorとかも設定できるようにしようかな
 
   /** 設定されたMapperやReducerの情報を元にいろいろ設定したJobを返す */
-  def createJob(jobName: String, inputPath: String, outputPath: String, conf: Configuration = getConf()): Job = {
-    val job = new Job(conf, jobName)
+  def createJob( jobName: String, inputPath: String, outputPath: String, conf: Configuration = getConf() ): Job = {
+    val job = new Job( conf, jobName )
 
-    FileInputFormat.setInputPaths(job, inputPath)
-    FileOutputFormat.setOutputPath(job, new Path(outputPath))
+    FileInputFormat.setInputPaths( job, inputPath )
+    FileOutputFormat.setOutputPath( job, new Path( outputPath ) )
 
-    job.setJarByClass(mapper.getClass())
-    job.setMapperClass(mapper.getClass())
+    job.setJarByClass( mapper.getClass() )
+    job.setMapperClass( mapper.getClass() )
 
-    if (reducer != null) {
-      job.setOutputKeyClass(reducer.outputKeyClass)
-      job.setOutputValueClass(reducer.outputValueClass)
-      job.setReducerClass(reducer.getClass())
-    } else {
-      job.setOutputKeyClass(mapper.outputKeyClass)
-      job.setOutputValueClass(mapper.outputValueClass)
+    if ( reducer != null ) {
+      job.setOutputKeyClass( reducer.outputKeyClass )
+      job.setOutputValueClass( reducer.outputValueClass )
+      job.setReducerClass( reducer.getClass() )
+    }
+    else {
+      job.setOutputKeyClass( mapper.outputKeyClass )
+      job.setOutputValueClass( mapper.outputValueClass )
     }
 
-    if (combiner != null) {
-      job.setCombinerClass(combiner.getClass())
+    if ( combiner != null ) {
+      job.setCombinerClass( combiner.getClass() )
     }
-
-    setInputFormat(job)
-    setOutputFormat(job)
 
     job
   }
 
-  /** setInputFormatClassを実行 */
-  def setInputFormat(job: Job) = job.setInputFormatClass(classOf[TextInputFormat])
-
-  /** setOutputFormatClassを実行 */
-  def setOutputFormat(job: Job) =
-    job.setOutputFormatClass(classOf[TextOutputFormat[_, _]])
-
   /** Jobの実行。成功したら0、失敗したら1を返す。 */
-  def run(job: Job): Int = {
-    val success = job.waitForCompletion(true)
-    if (success) 0 else 1
+  def run( job: Job ): Int = {
+    val success = job.waitForCompletion( true )
+    if ( success ) 0 else 1
   }
 
   /** Jobの完了を待つ。成功したら0、失敗したら1を返す。 */
-  def waitFor(job: Job, termSec: Int = 30): Int = {
-    while (job.isComplete()) {
-      Thread.sleep(termSec * 1000)
+  def waitFor( job: Job, termSec: Int = 30 ): Int = {
+    while ( job.isComplete() ) {
+      Thread.sleep( termSec * 1000 )
     }
-    if (job.isSuccessful()) 0 else 1
+    if ( job.isSuccessful() ) 0 else 1
   }
 }
