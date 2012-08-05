e66728045befb4d0ae4758018f3c4ad486b58ee4
mwsoft <msorwk@yahoo.co.jp>
2012-08-06 02:02:54 +0900

    add canopy sample

---
+++ mahout/src/main/java/jp/mwsoft/sample/mahout/canopy/Canopy1.java
@@ -0,0 +1,20 @@
+package jp.mwsoft.sample.mahout.canopy;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.mahout.clustering.canopy.CanopyDriver;
+import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
+
+public class Canopy1 {
+
+	public static void main(String[] args) throws Exception {
+		
+		CanopyDriver.run(new Path("data/canopy-sample-vector"), 
+				new Path("data/canopy-output2"), 
+				new EuclideanDistanceMeasure(),
+				2.0, 4.0, 
+				true, 
+				0.0,
+				false);
+		
+	}
+}
---
+++ mahout/src/main/java/jp/mwsoft/sample/mahout/canopy/Kmeans1.java
@@ -0,0 +1,18 @@
+package jp.mwsoft.sample.mahout.canopy;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.mahout.clustering.kmeans.KMeansDriver;
+import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
+
+public class Kmeans1 {
+
+	public static void main(String[] args) throws Exception {
+
+		KMeansDriver.run(new Path("data/canopy-sample-vector"),
+				new Path("data/canopy-output/clusters-0-final"),
+				new Path("data/canopy-kmeans-output"),
+				new EuclideanDistanceMeasure(),
+				0.001, 10, true, 0.0, false);
+	}
+
+}
